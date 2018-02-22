c973dc3519ff061fa240395473de0b37564cb87b
shuang286 <36724126+shuang286@users.noreply.github.com>
2018-02-22 00:36:20 -0800

    Update AtLarge.java

--- AtLarge.java
+++ AtLarge.java
@@ -2,16 +2,19 @@ import java.util.*;
 import java.io.*;
 
 /*
-In this tree problem we go from the leaves (dead ends) of the entire barn tree inwards, mapping every barn
+
+In this tree problem, we go from the leaves (dead ends) of the entire barn tree inwards, mapping every barn
 with a number using recursion. I was effectively able to give the least number to every internal node by starting from
 every leaf, and branching inwards from every leaf. That number represented the least number of barns it would
 take to reach the leaves (distance from the nearest leaf).
 
 Then we give every barn/node another number, the number of barns away from where the cow Bessie initially started
-from (distance from Bessie's original position). To count how many farmers are needed to effectively corner Bessie,
+from (distance from Bessie's original position). To count how many farmers are needed to effectively catch Bessie,
 I just simply compared the two numbers (the distance from the nearest leaf and the distance from Bessie's initial position).
-If a barn's distance from the nearest leaf was either the same or one less than the distance from
-Bessie's original position, then I knew we needed a farmer, adding one to the final count.
+If a barn's distance from the nearest leaf was either the same or one less than the distance from Bessie's original
+position, then I knew we needed a farmer, adding one to the final count. This "the same or one less than" condition
+is ensured by (b >= c.f) and "return" in method dfs2 (depth-first search, cow Bessie moves from the original position).
+
 */
 
 class Path {
@@ -107,6 +110,4 @@ public class AtLarge {
 //		writer.println(positions.size());
 //		writer.close();
 	
-
 }
-
